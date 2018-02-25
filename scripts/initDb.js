80b6934e2421e37b2a604a147cf5c2066a4c7320
Jori Lallo <jtlallo@gmail.com>
2018-02-25 01:00:31 -0800

    Removed iniDb script

--- scripts/initDb.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-require('@std/esm')(module);
-
-const Transaction = require('../src/models/transaction');
-Transaction.sync({ force: true }).then(() =>
-  console.log('Transaction table created')
-);
